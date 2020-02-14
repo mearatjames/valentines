3aec90da0b9d9ae718e5a925acd94dac2ff00d70
Mearat Hou <mearatjames@gmail.com>
2020-02-15 00:36:36 +1100

    Add favicon

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,16 +2,18 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import hero from '../hero.mp4'
 
 export default function Hero() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="lg">
-        <video autoPlay muted loop height="640" width="320" >
-          <source src="../hero.mp4" type="video/mp4"></source>
-        </video>
-      </Container>
+      <div className="videoContainer">
+            <video autoPlay="true" muted loop="true" id="myVideo">
+              <source src={hero} type="video/mp4" />
+           </video>
+        <Typography className="heroContent">Happy Valentine's Day Bor Bor</Typography>
+      </div>
     </React.Fragment>
   );
 }
